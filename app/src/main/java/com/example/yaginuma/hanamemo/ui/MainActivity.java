a230600c39e760df81e03568ad631a448677ff45
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-09-03 08:46:59 +0900

    add get location sample

--- app/src/main/java/com/example/yaginuma/hanamemo/ui/MainActivity.java
+++ app/src/main/java/com/example/yaginuma/hanamemo/ui/MainActivity.java
@@ -1,39 +1,84 @@
 package com.example.yaginuma.hanamemo.ui;
 
+import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yaginuma.hanamemo.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private TextView mLocationInfoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_show);
+        buildGoogleApiClient();
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        mLocationInfoView = (TextView)findViewById(R.id.exitInfo);
+     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_show, menu);
+            return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+            //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            String locationInfo = "latitude: " + String.valueOf(mLastLocation.getLatitude()) + " longitude: " + String.valueOf(mLastLocation.getLongitude());
+            mLocationInfoView.setText(locationInfo);
+        }
+    }
+
+    @Override
+    public void  onConnectionSuspended (int cause) {
+        Toast.makeText(this, "connection suspneded cause: " + cause, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "connection failed", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/yaginuma/hanamemo/ui/ShowActivity.java
+++ app/src/main/java/com/example/yaginuma/hanamemo/ui/ShowActivity.java
@@ -1,6 +1,7 @@
 package com.example.yaginuma.hanamemo.ui;
 
 import android.content.Intent;
+import android.location.Location;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -12,20 +13,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.yaginuma.hanamemo.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
-public class ShowActivity extends AppCompatActivity {
+public class ShowActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private TextView mLocationInfoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        buildGoogleApiClient();
 
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
+        mLocationInfoView = (TextView)findViewById(R.id.exitInfo);
 
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if (type.startsWith("image/")) {
@@ -37,6 +48,20 @@ public class ShowActivity extends AppCompatActivity {
             }
         } else {
             // Handle other intents, such as being started from the home screen
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
@@ -85,11 +110,38 @@ public class ShowActivity extends AppCompatActivity {
             String latitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
             String longitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
             String datetime = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
-            TextView textView = (TextView)findViewById(R.id.exitInfo);
-            textView.setText("datetime: " + datetime + " latitude: " + latitude + " longitude: " + longitude);
+            mLocationInfoView.setText("datetime: " + datetime + " latitude: " + latitude + " longitude: " + longitude);
         } catch(Exception e) {
             Toast.makeText(this, "occur error when loadinf exif", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            String locationInfo = "latitude: " + String.valueOf(mLastLocation.getLatitude()) + " longitude: " + String.valueOf(mLastLocation.getLongitude());
+            mLocationInfoView.setText(locationInfo);
+        }
+    }
+
+    @Override
+    public void  onConnectionSuspended (int cause) {
+        Toast.makeText(this, "connection suspneded cause: " + cause, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "connection failed", Toast.LENGTH_LONG).show();
     }
 }
